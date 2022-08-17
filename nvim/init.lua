2edd7e06d18cd4101cf290ab44d79a1511d5987e
Zhichong Fang <fzc621@gmail.com>
2022-08-17 10:34:35 -0700

    Add snippets

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,7 @@ require("user.keymaps")
 require("user.plugins")
 
 require("telescope").load_extension("fzf")
+require("luasnip/loaders/from_vscode").lazy_load()
 
 require("user.colorscheme")
 require("user.lsp")
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -143,6 +143,7 @@ return packer.startup(function(use)
 
     use("L3MON4D3/LuaSnip")
     use("saadparwaiz1/cmp_luasnip")
+    use "rafamadriz/friendly-snippets"
     use("hrsh7th/cmp-path")
     use("hrsh7th/cmp-cmdline")
     use({
