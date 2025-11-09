711cd02c0cf87beba16efa749669184fda976fd5
Zhichong Fang <fzc621@gmail.com>
2025-11-08 17:46:31 -0800

    use lazy vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,2 @@
-pcall(require, "impatient")
-
-require("user.options")
-require("user.keymaps")
-require("user.plugins")
-
-require("telescope").load_extension("fzf")
-require("luasnip/loaders/from_vscode").lazy_load()
-
-require("user.colorscheme")
-require("user.lsp")
-require("user.toggleterm")
-require("user.lsp_lines")
-
-pcall(require, "user.local")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
