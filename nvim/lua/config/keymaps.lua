cd526cb688f11ee3749a62f15cd2f71a8106f81d
Zhichong Fang <fzc621@gmail.com>
2025-11-20 23:57:40 -0800

    feat: Update Neovim configuration and tmux settings
    - Enable new LazyVim extras.
    - Map 'jk' to escape in Neovim insert mode.
    - Refine tmux terminal settings for improved color support.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Map 'jk' to <Esc> in insert mode
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode", noremap = true, silent = true })
